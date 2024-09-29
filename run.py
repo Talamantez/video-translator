94e9675b9ddcf5fcf7b3f2a11d8339222877fb19
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-09-29 15:54:33 -0700

    WIP - heroku deployment

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # This allows your app to work both locally and on Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
