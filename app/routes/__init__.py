fbf8da37a3ab68d8b038e0948d76cc581a88d0b0
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-10-26 21:25:10 -0700

    update to use nixpacks config

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,17 +1,3 @@
-from flask import Flask
 from .main_routes import main_bp
 from .process_routes import process_bp
 from .result_routes import result_bp
-
-def create_app():
-    app = Flask(__name__)
-    
-    # Configure app settings if needed
-    app.config['SECRET_KEY'] = 'your-secret-key-here'  # You should use an environment variable for this
-    
-    # Register blueprints
-    app.register_blueprint(main_bp)
-    app.register_blueprint(process_bp)
-    app.register_blueprint(result_bp)
-    
-    return app
